ae80064d793689fbb8e223ac29d5255267c90ceb
Govardhan Mathi <Govardhan.Mathi@bestbuy.com>
2017-09-07 15:40:49 -0500

    Commented realm code

--- app/src/main/java/io/github/gmathi/novellibrary/model/realm/RMetadata.kt
+++ app/src/main/java/io/github/gmathi/novellibrary/model/realm/RMetadata.kt
@@ -1,11 +1,11 @@
-package io.github.gmathi.novellibrary.model.realm
-
-import io.realm.RealmObject
-
-
-open class RMetadata: RealmObject() {
-
-    var key: String = ""
-    var value: String? = null
-
-}
+//package io.github.gmathi.novellibrary.model.realm
+//
+//import io.realm.RealmObject
+//
+//
+//open class RMetadata: RealmObject() {
+//
+//    var key: String = ""
+//    var value: String? = null
+//
+//}
