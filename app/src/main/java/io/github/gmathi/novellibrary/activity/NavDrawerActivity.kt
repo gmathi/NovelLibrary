f777cb7deaa23f164553324d292bc0aff5bd6291
Guiorgy <Guiorgy123@gmail.com>
2020-01-14 19:42:37 +0400

    refactor

--- app/src/main/java/io/github/gmathi/novellibrary/activity/NavDrawerActivity.kt
+++ app/src/main/java/io/github/gmathi/novellibrary/activity/NavDrawerActivity.kt
@@ -5,35 +5,32 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
-import com.google.android.material.navigation.NavigationView
-import com.google.android.material.snackbar.Snackbar
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import androidx.core.view.GravityCompat
-import androidx.appcompat.widget.Toolbar
-import android.view.MenuItem
-import android.view.ViewTreeObserver
 import co.metalab.asyncawait.async
 import com.afollestad.materialdialogs.MaterialDialog
 import com.crashlytics.android.Crashlytics
-import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
 import io.fabric.sdk.android.Fabric
 import io.github.gmathi.novellibrary.BuildConfig
 import io.github.gmathi.novellibrary.R
 import io.github.gmathi.novellibrary.dataCenter
+import io.github.gmathi.novellibrary.extensions.*
 import io.github.gmathi.novellibrary.fragment.LibraryPagerFragment
 import io.github.gmathi.novellibrary.fragment.SearchFragment
 import io.github.gmathi.novellibrary.model.Novel
 import io.github.gmathi.novellibrary.util.Constants
+import io.github.gmathi.novellibrary.util.Logs
 import io.github.gmathi.novellibrary.util.Utils
 import kotlinx.android.synthetic.main.activity_nav_drawer.*
 import kotlinx.android.synthetic.main.app_bar_nav_drawer.*
-import kotlinx.android.synthetic.main.nav_header_nav_drawer.*
 import org.cryse.widget.persistentsearch.PersistentSearchView
-import com.google.firebase.auth.FirebaseAuth
-import io.github.gmathi.novellibrary.extensions.*
-import io.github.gmathi.novellibrary.util.Logs
 
 
 class NavDrawerActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
