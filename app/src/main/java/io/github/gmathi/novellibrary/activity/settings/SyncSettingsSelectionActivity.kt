9ce7365969b4d13fa8e7521e83ed2e52de3ca4f0
Fionn Langhans <fionn.langhans@gmail.com>
2020-12-29 11:20:54 +0100

    Migrated from synthetics to view binding in SyncSettingsSelectionActivity

--- app/src/main/java/io/github/gmathi/novellibrary/activity/settings/SyncSettingsSelectionActivity.kt
+++ app/src/main/java/io/github/gmathi/novellibrary/activity/settings/SyncSettingsSelectionActivity.kt
@@ -9,26 +9,29 @@ import io.github.gmathi.novellibrary.R
 import io.github.gmathi.novellibrary.activity.BaseActivity
 import io.github.gmathi.novellibrary.adapter.GenericAdapter
 import io.github.gmathi.novellibrary.dataCenter
+import io.github.gmathi.novellibrary.databinding.ActivitySettingsBinding
+import io.github.gmathi.novellibrary.databinding.ListitemTitleSubtitleBinding
 import io.github.gmathi.novellibrary.util.system.startSyncSettingsActivity
 import io.github.gmathi.novellibrary.network.HostNames
 import io.github.gmathi.novellibrary.network.sync.NovelSync
 import io.github.gmathi.novellibrary.util.view.CustomDividerItemDecoration
 import io.github.gmathi.novellibrary.util.applyFont
 import io.github.gmathi.novellibrary.util.setDefaults
-import kotlinx.android.synthetic.main.activity_settings.*
-import kotlinx.android.synthetic.main.content_recycler_view.*
-import kotlinx.android.synthetic.main.listitem_title_subtitle.view.*
 import java.util.ArrayList
 
 class SyncSettingsSelectionActivity : BaseActivity(), GenericAdapter.Listener<String> {
 
     lateinit var adapter: GenericAdapter<String>
     private lateinit var settingsItems: ArrayList<String>
+    
+    private lateinit var binding: ActivitySettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-        setSupportActionBar(toolbar)
+        
+        binding = ActivitySettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setRecyclerView()
     }
@@ -41,13 +44,14 @@ class SyncSettingsSelectionActivity : BaseActivity(), GenericAdapter.Listener<St
     private fun setRecyclerView() {
         settingsItems = ArrayList(resources.getStringArray(R.array.sync_list).asList())
         adapter = GenericAdapter(items = settingsItems, layoutResId = R.layout.listitem_title_subtitle, listener = this)
-        recyclerView.setDefaults(adapter)
-        recyclerView.addItemDecoration(CustomDividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-        swipeRefreshLayout.isEnabled = false
+        binding.contentRecyclerView.recyclerView.setDefaults(adapter)
+        binding.contentRecyclerView.recyclerView.addItemDecoration(CustomDividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        binding.contentRecyclerView.swipeRefreshLayout.isEnabled = false
     }
 
     override fun bind(item: String, itemView: View, position: Int) {
-        itemView.title.applyFont(assets).text = item
+        val itemBinding = ListitemTitleSubtitleBinding.bind(itemView)
+        itemBinding.title.applyFont(assets).text = item
         val host = when(item) {
             getString(R.string.source_novel_updates) -> HostNames.NOVEL_UPDATES
             else -> HostNames.NOVEL_UPDATES
@@ -55,14 +59,14 @@ class SyncSettingsSelectionActivity : BaseActivity(), GenericAdapter.Listener<St
 
         if (position == 0) {
             // Disclaimer
-            itemView.subtitle.applyFont(assets).text = getString(R.string.experimental_sync_description)
-            itemView.chevron.setImageResource(R.drawable.ic_info_white_vector)
-            itemView.chevron.imageTintList = ContextCompat.getColorStateList(this, R.color.colorStateBlue)
+            itemBinding.subtitle.applyFont(assets).text = getString(R.string.experimental_sync_description)
+            itemBinding.chevron.setImageResource(R.drawable.ic_info_white_vector)
+            itemBinding.chevron.imageTintList = ContextCompat.getColorStateList(this, R.color.colorStateBlue)
         } else {
             val loggedIn = getString(if (NovelSync.getInstance(host, true)?.loggedIn() == true) R.string.logged_in else R.string.not_logged_in)
             val enabled = getString(if(dataCenter.getSyncEnabled(host)) R.string.enabled else R.string.disabled)
 
-            itemView.subtitle.applyFont(assets).text = getString(R.string.sync_status_description, loggedIn, enabled)
+            itemBinding.subtitle.applyFont(assets).text = getString(R.string.sync_status_description, loggedIn, enabled)
         }
 
         itemView.setBackgroundColor(if (position % 2 == 0) ContextCompat.getColor(this, R.color.black_transparent)
