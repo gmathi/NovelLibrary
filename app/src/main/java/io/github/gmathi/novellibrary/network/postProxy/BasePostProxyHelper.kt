c8ef36455ccbb6fb17f7550cac7d2af97fb14fc7
Yanrishatum <krabanek@gmail.com>
2022-02-17 17:31:49 +0300

    Improve intl post-chapter nagger detection Add more characters to repetition detection.

--- app/src/main/java/io/github/gmathi/novellibrary/network/postProxy/BasePostProxyHelper.kt
+++ app/src/main/java/io/github/gmathi/novellibrary/network/postProxy/BasePostProxyHelper.kt
@@ -35,7 +35,7 @@ open class BasePostProxyHelper {
                             val json: INovelTranslationJson = Gson().fromJson(jsonString)
                             val raw = json.content
                                 // Hide obnoxious T/N notes in TTS.
-                                .replace("""---\s*T/N:.+(\n\[?!$|---)""".toRegex(setOf(RegexOption.DOT_MATCHES_ALL)), "<div tts-disable=\"true\">\n\n$0\n\n</div>")
+                                .replace("""---\s*(T/N:|Hello!\s?\?\?+).+(\n\[!$|---)""".toRegex(setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.MULTILINE)), "<div tts-disable=\"true\">\n\n$0\n\n</div>")
                                 .replace("""T/N:.+shoutout to the following:.*?for being.*?patre?ons?.*?$""".toRegex(setOf(RegexOption.DOT_MATCHES_ALL, RegexOption.MULTILINE)), "<div tts-disable=\"true\">\n\n$0\n\n</div>")
                                 .replace("""(Editor|Translator|T/N): .+""".toRegex(), "<p tts-disable=\"true\">$0</p>")
                             val html = raw.let {
